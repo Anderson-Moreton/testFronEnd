0a16fc323f819f47043140a002ee0ae3706559a9
Anderson Moreton <anderson.moreton@gmail.com>
2025-12-17 23:45:10 +0000

    Create projet change the color of a balloon

---
+++ 42Porto/cell03/ex01/script.js
@@ -0,0 +1,34 @@
+// Select the balloon element
+const bgBalloon = document.querySelector('.balloonRed');
+
+// Array of colors to change
+const colors = ['#065314ff','#143cf1ff', '#f52d00ff',]
+let indexColor = 0;
+
+// Function to change balloon color
+function changeBalloonColor() {
+    bgBalloon.style.backgroundColor = colors[indexColor]; // Set new color
+    indexColor = (indexColor + 1) % colors.length; // Cycle through colors
+}
+
+function explodeBalloon() { // Increase size of the balloon on each click
+    bgBalloon.style.height = (bgBalloon.offsetHeight + 10) + 'px'; // Increase height
+    bgBalloon.style.width = (bgBalloon.offsetWidth + 10) + 'px'; // Increase width
+    if (bgBalloon.offsetHeight >= 420) { 
+        alert("💥 The balloon exploded! 💥"); 
+        bgBalloon.style.height = '200px'; // Reset size
+        bgBalloon.style.width = '200px'; // Reset size
+        bgBalloon.style.backgroundColor = '#f52d00ff'; // Reset color
+    }
+}
+
+// Event listener for button click
+bgBalloon.addEventListener('click', () => { // On balloon click
+    changeBalloonColor(); // Change color on each click
+    explodeBalloon(); // Increase size and check for explosion
+});
+
+// Initial color setup
+changeBalloonColor();
+
+
